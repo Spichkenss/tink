857a8949acf6f0f6568bb4591ae3ece322ff2336
spichkenss <spichkens.2002@mail.ru>
2024-04-03 15:53:57 +0300

    error

---
+++ src/app/(pages)/[locale]/error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+
+const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        type="button"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+};
