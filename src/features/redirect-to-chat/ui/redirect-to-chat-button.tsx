6aca733a8ffe663ed9f5915627521f4ba7dd9392
spichkenss <spichkens.2002@mail.ru>
2024-04-03 18:02:52 +0300

    contancts

--- src/features/redirect-to-chat/ui/redirect-to-chat-button.tsx
+++ src/features/redirect-to-chat/ui/redirect-to-chat-button.tsx
@@ -1,9 +1,14 @@
+import { prisma } from "@/shared/config/prisma";
 import { Button } from "@/shared/ui/button";
 
-export const RedirectToChatButton = () => {
+export const RedirectToChatButton = async () => {
+  const chat = await prisma.contacts.findFirst({ where: { name: "WHATSUP" } });
+
   return (
     <Button className="p-0 m-0" variant="link">
-      Возникли вопросы?
+      <a href={chat?.link}>
+        Возникли вопросы?
+      </a>
     </Button>
   );
 };
