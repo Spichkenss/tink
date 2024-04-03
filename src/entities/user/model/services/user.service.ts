a248fab9f8c19b135691bd5ac7ef636df317b174
spichkenss <spichkens.2002@mail.ru>
2024-04-03 16:07:01 +0300

    fix

--- src/entities/user/model/services/user.service.ts
+++ src/entities/user/model/services/user.service.ts
@@ -3,12 +3,12 @@ import { Phone, SharedUser, UserId } from "@/shared/domain/user/types";
 import { userRepository } from "../repositories/user-repository";
 
 class UserService {
-  async getById(id: UserId): Promise<SharedUser> {
-    return userRepository.findUniqueOrThrow({ where: { id } });
+  async getById(id: UserId): Promise<SharedUser | null> {
+    return userRepository.findUnique({ where: { id } });
   }
 
   async getByPhone(phone: Phone): Promise<SharedUser | null> {
-    return userRepository.findUniqueOrThrow({ where: { phone } });
+    return userRepository.findUnique({ where: { phone } });
   }
 }
 
